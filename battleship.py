fb551066f66aa0f9919404c7621cf0fb777a2a79
Michelle Bergquist <caker25@gmail.com>
2016-11-04 16:07:04 -0600

    Refactor: player management
    Added lists for boards and ship locations. This allows us to change the number of players easily, and reduces the total amount of code.

--- battleship.py
+++ battleship.py
@@ -4,6 +4,8 @@ from random import randint
 ROWS = 5
 COLS = 5
 
+PLAYERS = 2
+
 def print_board(board):
     for row in board:
         print(" ".join(row))
@@ -18,39 +20,37 @@ def random_col(board):
 ## Actual Game Starts Here
 ## =======================
 
-# set up board
-player1board = []
-player2board = []
-for i in range(ROWS):
-    player1board.append(["O"] * COLS)
-    player2board.append(["O"] * COLS)
+# set up boards
+boards = []
+for i in range(PLAYERS):
+    board = []
+    for j in range(ROWS):
+        board.append(["0"] * COLS)
+    boards.append(board)
 
-ship_row_p1 = random_row(player1board)
-ship_col_p1 = random_col(player1board)
-ship_row_p2 = random_row(player2board)
-ship_col_p2 = random_col(player2board)
+ship_locs = []
+for i in range(PLAYERS):
+    ship_row = random_row(boards[i])
+    ship_col = random_col(boards[i])
+    ship_locs.append((ship_row, ship_col))
 
 
 
 print("Let's play Battleship!")
-print_board(player1board)
+print_board(boards[0])
 
-whos_turn = 1
+whos_turn = 0
 while True:
     # set up turn
-    if whos_turn == 1:
-        ship_row = ship_row_p1
-        ship_col = ship_col_p1
-        board_to_check = player1board
-    else:
-        ship_row = ship_row_p2
-        ship_col = ship_col_p2
-        board_to_check = player2board
+    location = ship_locs[whos_turn]
+    ship_row = location[0]
+    ship_col = location[1]
+    board_to_check = boards[whos_turn]
 
     # allow player to guess.  repeat until guess is valid
     while True:
-        guess_row = int(input("Player {} Guess Row: ".format(whos_turn)))
-        guess_col = int(input("Player {} Guess Col: ".format(whos_turn)))
+        guess_row = int(input("Player {} Guess Row: ".format(whos_turn + 1)))
+        guess_col = int(input("Player {} Guess Col: ".format(whos_turn + 1)))
         if (guess_row < 0 or guess_row >= ROWS) or (guess_col < 0 or guess_col >= COLS):
             print("Oops, that's not even in the ocean. Guess again.")
         elif (board_to_check[guess_row][guess_col] == "X"):
@@ -59,19 +59,20 @@ while True:
             break
 
     if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship! Player {} wins!!!".format(whos_turn))
+        print("Congratulations! You sunk my battleship! Player {} wins!!!".format(whos_turn + 1))
         break # stops game
     else:
         print("You missed my battleship!")
         board_to_check[guess_row][guess_col] = "X"
 
     # print board for player
-    print("Player {}'s board:'".format(whos_turn))
+    print("Player {}'s board:'".format(whos_turn + 1))
     print_board(board_to_check)
-    # change turns for next time in loop        
-    if whos_turn == 1:
-        whos_turn = 2
+    # change turns for next time in loop
+    if whos_turn == PLAYERS - 1:
+        whos_turn = 0
     else:
-        whos_turn = 1
+        whos_turn += 1
+
         
 print("Goodbye")
