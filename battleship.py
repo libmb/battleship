eb9d190b8abbdede566a4db6b6aa806f4dfa6046
Michelle Bergquist <caker25@gmail.com>
2016-11-18 18:16:06 -0700

    Add class for board.
    The board knows the location of its ship and has a guess method.

--- battleship.py
+++ battleship.py
@@ -2,50 +2,62 @@ from random import randint
 
 # size of the board
 ROWS = 5
-COLS = 5
+COLS = 6
 
 PLAYERS = 2
 
-def print_board(board):
-    for row in board:
-        print(" ".join(row))
 
-def random_row(board):
-    return randint(0, len(board) - 1)
+class Board(object):
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.board = []
+        self.ship_row = -1
+        self.ship_col = -1
+        for i in range(self.rows):
+            self.board.append(["0"] * self.cols)
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
+    def __repr__(self):
+        grid = ""
+        for row in self.board:
+            grid += " ".join(row) + "\n"
+        return grid
+
+    def place_ship(self):
+        self.ship_row = randint(0, len(self.board)-1)
+        self.ship_col = randint(0, len(self.board[0])-1)
+
+    def guess(self, guess_row, guess_col):
+        if guess_row == self.ship_row and guess_col == self.ship_col:
+            return "Hit"
+        elif self.board[guess_row][guess_col] == "X":
+            return "Try again"
+        else:
+            self.board[guess_row][guess_col] = "X"
+            return "Miss"
 
 
-## Actual Game Starts Here
-## =======================
+# Actual Game Starts Here
+# =======================
 
 # set up boards
-boards = []
+player_boards = []
 for i in range(PLAYERS):
-    board = []
-    for j in range(ROWS):
-        board.append(["0"] * COLS)
-    boards.append(board)
+    # create the boards
+    player_boards.append(Board(ROWS, COLS))
 
-ship_locs = []
-for i in range(PLAYERS):
-    ship_row = random_row(boards[i])
-    ship_col = random_col(boards[i])
-    ship_locs.append((ship_row, ship_col))
-
+for b in player_boards:
+    # place the ships
+    b.place_ship()
 
 
 print("Let's play Battleship!")
-print_board(boards[0])
+print(player_boards[0])
 
 whos_turn = 0
 while True:
     # set up turn
-    location = ship_locs[whos_turn]
-    ship_row = location[0]
-    ship_col = location[1]
-    board_to_check = boards[whos_turn]
+    board_to_check = player_boards[whos_turn]
 
     # allow player to guess.  repeat until guess is valid
     while True:
@@ -53,21 +65,20 @@ while True:
         guess_col = int(input("Player {} Guess Col: ".format(whos_turn + 1)))
         if (guess_row < 0 or guess_row >= ROWS) or (guess_col < 0 or guess_col >= COLS):
             print("Oops, that's not even in the ocean. Guess again.")
-        elif (board_to_check[guess_row][guess_col] == "X"):
-            print("You guessed that one already. Guess again.")
         else:
-            break
-
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk my battleship! Player {} wins!!!".format(whos_turn + 1))
-        break # stops game
-    else:
-        print("You missed my battleship!")
-        board_to_check[guess_row][guess_col] = "X"
+            result = board_to_check.guess(guess_row, guess_col)
+            if result == "Hit":
+                print("Congratulations! You sunk my battleship! Player {} wins!!!".format(whos_turn + 1))
+                exit()
+            elif result == "Miss":
+                print("You missed my battleship!")
+                break
+            else:
+                print("You already guess that. Try Again")
 
     # print board for player
     print("Player {}'s board:'".format(whos_turn + 1))
-    print_board(board_to_check)
+    print(board_to_check)
     # change turns for next time in loop
     if whos_turn == PLAYERS - 1:
         whos_turn = 0
